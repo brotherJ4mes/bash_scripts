2f1183fcd47f63128db96398aa437e6f00fc5a8d
james <38081401+brotherJ4mes@users.noreply.github.com>
2024-09-12 18:04:46 -0400

    updated daemon_unlock

--- daemon_unlock.sh
+++ daemon_unlock.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+
+notify-send 'starting unlock daemon now...'
+
+
+
 /bin/gdbus monitor -y -d org.freedesktop.login1 | \
 while read line; do 
 	if echo $line | grep -q Unlock; then 
 		sleep 1
-		notify-send 'Unlocked!'; # remove this later
+		notify-send `cat ~/.lastunlock`; 
 		/home/j4mes/my_scripts/unlock.sh
+	elif echo $line | grep -q Lock; then 
+		touch ~/locked_it
+		date '+%b-%d %H:%M' > ~/.lastunlock	
+		pkill vlc
+		pkill chrome
 	fi;  
 
 	# backdoor for runaways
